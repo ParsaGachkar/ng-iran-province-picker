368ad46bca7e87bff1e41daf08dca01c4c2dab69
Parsa Gachkar <p.gachkar@outlook.com>
2019-03-27 00:27:31 +0430

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MapProvince} from './ng-iran-province-picker/ng-iran-province-picker/ng-iran-province-picker.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IranMap';
+  Province = 'None'
+  log(p: MapProvince) {
+    console.log(p + ' Selected!');
+    this.Province = p;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgIranProvincePicker} from './ng-iran-province-picker/ng-iran-province-picker.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgIranProvincePicker
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
